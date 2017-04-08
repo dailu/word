0291a5763197ee3d6cc6e7218148b674f76c6e40
dailu <dai_lu@hotmail.com>
2017-04-08 14:33:54 +0800

    initial import

---
+++ common/models/note.js
@@ -0,0 +1,10 @@
+module.exports = function(Note) {
+	Note.greet = function(msg, cb) {
+	      cb(null, 'Greetings... ' + msg);
+	    }
+
+	    Note.remoteMethod('greet', {
+	          accepts: {arg: 'msg', type: 'string'},
+	          returns: {arg: 'greeting', type: 'string'}
+	    });
+};
