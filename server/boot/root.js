19118ccbc0077379166e02c155197525c38c53d5
dailu <dai_lu@hotmail.com>
2017-04-14 12:05:15 +0800

    test

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,8 +1,9 @@
 'use strict';
 
-module.exports = function(server) {
-  // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
-  server.use(router);
+module.exports = function(app) {
+	var router = app.loopback.Router();
+	
+
+   
+	app.use(router);
 };
